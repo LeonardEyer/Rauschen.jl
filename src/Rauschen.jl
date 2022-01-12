437664417038648e0a5662175a8df57f908c9ea4
Leonard Eyer <leonardeyer@fastmail.de>
2022-01-12 17:17:02 +0100

    Fix project name. Add boilerplate

---
+++ src/Rauschen.jl
@@ -0,0 +1,3 @@
+module Rauschen
+    # Empty
+end
---
+++ test/runtests.jl
@@ -0,0 +1,14 @@
+using Rauschen
+using Test
+
+# Tests will be added here
+@testset "Basic Tests" begin
+    @testset "Test1" begin
+        @testset "Part 1" begin
+            @assert true
+        end
+        @testset "Part 2" begin
+            @assert true
+        end
+    end
+end
